c854d1a2153238d70ea19d5fd8d46d1ae0da8b72
paulbrochard <paul.brochard@papernest.com>
2023-06-30 11:05:30 +0200

    Manage project

--- src/simplelogin/exceptions.py
+++
@@ -1,6 +0,0 @@
-
-
-class SimpleLoginError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(message)
--- src/simplelogin/settings.py
+++
@@ -1,3 +0,0 @@
-BASE_URL = "https://app.simplelogin.io"
-TOKEN_ENV_VARIABLE_NAME = "SIMPLELOGIN_TOKEN"
-TOKEN = ""
