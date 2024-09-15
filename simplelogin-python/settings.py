554b639b3cbcb62ea9918e9591fb40a04d56e6f3
lionel-meaudre <lionel.meaudre@papernest.com>
2024-09-15 14:26:42 +0200

    Use .env to persist token

--- simplelogin-python/settings.py
+++ simplelogin-python/settings.py
@@ -1,4 +1,3 @@
 BASE_URL = "https://app.simplelogin.io"
 TOKEN_ENV_VARIABLE_NAME = "SIMPLELOGIN_TOKEN"
-TOKEN = ""
 VERIFY_SSL = False
--- simplelogin-python/simple_login.py
+++ simplelogin-python/simple_login.py
@@ -4,6 +4,7 @@ import utils.connector as connector
 import settings
 import warnings
 from services.aliases_service import AliasesService
+from utils.environment import get_token, save_token
 from utils.output import pretty_print_aliases, print_alias, print_aliases
 
 app = typer.Typer()
@@ -12,13 +13,23 @@ if settings.VERIFY_SSL == False:
     warnings.filterwarnings('ignore')
 
 @app.command()
+def set_token(token: Annotated[
+        str,
+        typer.Argument(
+            help="The token created in the SimpleLogin dashboard",
+        )]):
+
+    save_token(token)
+    print("Token saved!")
+
+@app.command()
 def list(verbose: Annotated[
         bool,
         typer.Option(
             help="Display aliases properties in a table",
         )] = False):
     print(f"Listing all aliases")
-    service = AliasesService(settings.TOKEN)
+    service = AliasesService(get_token())
     if verbose:
         pretty_print_aliases(service.aliases)
     else:
@@ -26,8 +37,8 @@ def list(verbose: Annotated[
 
 @app.command()
 def create():
-    print(f"Creating a new random aliases")
-    service = AliasesService(settings.TOKEN)
+    print(f"Creating a new random alias")
+    service = AliasesService(get_token())
     alias = service.new_random_alias()
     print_alias(alias)
 
@@ -38,7 +49,7 @@ def delete(id: Annotated[
             help="The id of the alias to delete",
         )]):
     print(f"Deleting alias #{id}")
-    service = AliasesService(settings.TOKEN)
+    service = AliasesService(get_token())
     alias = service.get_alias(id)
     if alias == None:
         print(f"Alias #{id} not found")
